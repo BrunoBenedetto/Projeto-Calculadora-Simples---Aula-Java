6039a69657dac7ebd145ff370a3603ef0903f3c9
BrunoBenedetto <bruno7.benedetto@gmail.com>
2026-04-10 14:00:11 -0400

    Melhoria na calculadora

--- src/program/Main.java
+++ src/program/Main.java
@@ -8,42 +8,46 @@ public class Main {
 
         Locale.setDefault(Locale.US);
 
-        double num1 = Double.parseDouble(JOptionPane.
-                showInputDialog(null, "Insira o primeiro número"));
+        for (;;) {
+            double num1 = Double.parseDouble(JOptionPane.
+                    showInputDialog(null, "Insira o primeiro número"));
 
-        String operation = JOptionPane.showInputDialog(null, """
-                Selecione a operação:
-                + (SOMA)
-                - (SUBTRAÇÃO)
-                * (MULTIPLICAÇÃO)
-                / (DIVISÃO)""");
+            String operation = JOptionPane.showInputDialog(null, """
+                    Selecione a operação:
+                    + (SOMA)
+                    - (SUBTRAÇÃO)
+                    * (MULTIPLICAÇÃO)
+                    / (DIVISÃO)""");
 
-        double num2 = Double.parseDouble(JOptionPane.
-                showInputDialog(null, "Insira o segundo número"));
+            double num2 = Double.parseDouble(JOptionPane.
+                    showInputDialog(null, "Insira o segundo número"));
 
-        double result = 0;
+            double result = 0;
 
-        switch (operation) {
-            case "+":
-                result = num1 + num2;
-                break;
+            switch (operation) {
+                case "+":
+                    result = num1 + num2;
+                    break;
 
-            case "-":
-                result = num1 - num2;
-                break;
+                case "-":
+                    result = num1 - num2;
+                    break;
 
-            case "*":
-                result = num1 * num2;
-                break;
+                case "*":
+                    result = num1 * num2;
+                    break;
 
-            case "/":
-                if (num1 == 0 || num2 == 0){
-                    JOptionPane.showMessageDialog(null, "Não é possível dividir por 0");
-                }
-                result = num1 / num2;
-                break;
+                case "/":
+                    if (num2 == 0) {
+                        JOptionPane.showMessageDialog(null, "Não é possível dividir por 0");
+                    }
+                    result = num1 / num2;
+                    break;
 
+                default:
+                    JOptionPane.showMessageDialog(null, "Operação Inválida");
+            }
+            JOptionPane.showMessageDialog(null, "O resultado é: \n" + result);
         }
-        JOptionPane.showMessageDialog(null, result);
     }
 }
